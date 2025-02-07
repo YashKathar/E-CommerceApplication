4b4082fc241a01f76035f01d5f0b3e10d66462ac
YashKathar <134498488+YashKathar@users.noreply.github.com>
2025-02-07 12:45:12 +0530

    Delete src directory

--- src/js/privateRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { toast } from "react-toastify";
-
-function PrivateRoute({element : Component}) {
-    const jwtToken = localStorage.getItem('jwtToken');
-    return jwtToken ? <Component/> : <Navigate to="/login"/>
-}
-
-export default PrivateRoute;
